d3203636cd1643b8ba4a9dab293f54fa7c4ae2d6
trinhnguyen1810 <trinhnguyen@uni.minerva.edu>
2024-10-11 00:34:46 -0700

    Initial Commit

---
+++ CookAid/CookAidApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookAidApp.swift
+//  CookAid
+//
+//  Created by Vivian Nguyen on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CookAidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
